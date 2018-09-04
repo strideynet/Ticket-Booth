a75d6dfeec7b1b94213e11c522778b33d4eb54af
Unknown <mrjackkefford@gmail.com>
2018-09-04 17:39:24 +0100

    Mobile Optimization
    Fiddle with text size

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -4,13 +4,13 @@
           <v-card-text>
             <v-layout align-center>
               <v-flex class="text-xs-center">
-                <h3 class="display-3">Welcome to the big bike bash ticket portal</h3>
-
-                <span class="subheading">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</span>
-
+                <h3 class="display-1">Welcome to the Big Bike Bash ticket portal</h3>
+                <div class="text-xs-left">
+                  <span class="subheading">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</span>
+                </div>
                 <v-divider class="my-3"></v-divider>
 
-                <div class="title mb-3">Tickets sold</div>
+                <div class="title mb-3">Tickets sold ({{currentParticipants}}/{{maxParticipants}})</div>
                 <v-progress-linear 
                   :value = "percentageSold"
                 >
