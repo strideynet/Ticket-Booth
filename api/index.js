245dda53a130c4a9c7e0001405f660d6c16526f2
Unknown <noah.stride@selfsown.com>
2018-09-16 15:59:21 +0100

    Bugsnag

--- api/index.js
+++ api/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const router = require('./router')
-
+const bugsnag = require('bugsnag')
 const app = express()
+
+bugsnag.register('8729333ba977a064080331f47da7d76c')
+
+app.use(bugsnag.requestHandler)
+app.use(bugsnag.errorHandler)
 
 app.use(express.json())
 app.use('/api', router)
