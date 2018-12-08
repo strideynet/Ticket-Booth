11e12c6b2f6a8cf17ce47b70953fe824b78b78eb
Noah Stride <noah.stride@selfsown.com>
2018-12-08 13:24:58 +0000

    Ready for cloud stackdriver error reporting

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
-const bugsnag = require('bugsnag')
 const cors = require('cors')
 const config = require('config')
-const debug = require('debug')('ticket-boot:entry')
+const debug = require('debug')('ticket-booth:entry')
 const db = require('./db')
 const errors = require('./helpers/errors')
 const express = require('express')
 const router = require('./router')
 
 const app = express()
-
-const BUGSNAG = config.get('bugsnag')
-
-if (process.env.NODE_ENV === 'production' && BUGSNAG) {
-  bugsnag.register(BUGSNAG, {})
-
-  app.use(bugsnag.requestHandler)
-  app.use(bugsnag.errorHandler)
-}
 
 app.use(cors())
 app.options('*', cors())
