814a998ec3051a455b2d1326cd75b2b34c340dca
Noah Stride <noah@noahstride.co.uk>
2019-08-11 11:37:57 +0100

    Join in order

--- routes/participants/get.js
+++ routes/participants/get.js
@@ -1,7 +1,9 @@
 const db = require('../../db')
 
 module.exports = (req, res, next) => {
-  db.models.Participant.findAll({})
+  db.models.Participant.findAll({
+    include: [ db.models.Order ]
+  })
     .then(participants => {
       res.status(200)
         .json(participants)
