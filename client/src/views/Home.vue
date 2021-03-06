fb131f526eb53c5938e107e3035cf05c968e1fcb
Noah Stride <noah@noahstride.co.uk>
2021-03-06 13:59:09 +0000

    final prep
    Signed-off-by: Noah Stride <noah@noahstride.co.uk>

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -14,9 +14,8 @@
 
             <div class="text-xs-left">
               <span class="subheading">
+                <h2>Sorry, this round of sales is for 2020 ticket holders only! If you are not a 2020 ticket holder, we will cancel your order! General sales will open later in March.</h2>
                 Big Bike Bash is returning for 2021. We hope you are looking forward to it as much as we are with another year of fun for the whole family.
-                <br><br>
-                <h2>This round of sales is for 2020 ticket holders only! If you are not a 2020 ticket holder, we will cancel your order!</h2>
                 <br><br>
                 Should you have any difficulties using the site, please contact admin@bigbikebash.org.uk
               </span>
--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
   maxParticipants: 750,
-  salesOpen: '2021-08-30 13:00:00',
+  salesOpen: '2021-03-06 14:05:00',
   bashDate: '2021-08-30'
 }
