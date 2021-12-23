ee0192114c0f8ef14fe1299c6fedae11f1f41385
Noah Stride <noah@noahstride.co.uk>
2021-12-23 12:38:42 +0000

    tweak: change override code

--- client/src/modules/store.js
+++ client/src/modules/store.js
@@ -83,7 +83,7 @@ export default {
         return false
       }
 
-      if (!getters.isSalesOpen && state.overrideCode !== 'glugglugglug`') {
+      if (!getters.isSalesOpen && state.overrideCode !== 'ticketsforcheese`') {
         return false
       }
 
