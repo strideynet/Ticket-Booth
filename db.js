59acf8eb549b2d1e8e40c73bbd13ae73b2ca40e1
Noah Stride <noah@noahstride.co.uk>
2022-04-03 15:50:26 +0100

    support connecting to socket path

--- db.js
+++ db.js
@@ -5,25 +5,37 @@ const logger = require('pino')({
 })
 const Sequelize = require('sequelize')
 
+const DB_INSTANCE_CONNECTION_NAME = config.get('db.instanceConnectionName')
+const DB_SOCKET_PATH = config.get('db.socketPath')
 const DB_HOST = config.get('db.host')
 const DB_DATABASE = config.get('db.database')
 const DB_USER = config.get('db.user')
 const DB_PASS = config.get('db.pass')
 const DB_PORT = config.get('db.port')
 
-/* Initialize database */
-const sequelize = new Sequelize({
-  host: DB_HOST,
+let options = {
   username: DB_USER,
   password: DB_PASS,
   database: DB_DATABASE,
-  port: DB_PORT,
   logging: logger.trace.bind(logger),
   dialect: 'mysql',
   dialectOptions: {
-    multipleStatements: true
+    multipleStatements: true,
   }
-})
+}
+
+if (DB_SOCKET_PATH) {
+  options.dialectOptions.socketPath = DB_SOCKET_PATH
+} else if (DB_INSTANCE_CONNECTION_NAME) {
+  options.dialectOptions.socketPath = `/cloudsql/${DB_INSTANCE_CONNECTION_NAME}`
+} else {
+  options.host = DB_HOST
+  options.port = DB_PORT
+}
+
+
+/* Initialize database */
+const sequelize = new Sequelize(options)
 
 /* Attempt connection to check details */
 sequelize
